865de5b2d832be429c17a77d65a6f20322b19f94
peakchao <zhangzhichaolove@vip.qq.com>
2020-07-04 14:43:20 +0800

    add web server.

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
+)
 
+func main() {
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+	app.Use(recover.New())
+	app.Use(logger.New())
+	app.Favicon("./assets/favicon.ico")
+	app.StaticWeb("/", "./ui")
+	app.RegisterView(iris.HTML("./ui", ".html").Reload(true))
+	app.Get("/", func(ctx iris.Context) {
+		ctx.View("index.html")
+	})
+	app.Done(func(ctx iris.Context) {})
+	app.Run(iris.Addr(":80"), iris.WithCharset("UTF-8"))
 }
